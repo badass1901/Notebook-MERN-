d4b0d52d09f464efa2ec403c1d85b999390639be
Shashwat Sagar <shashwatsagar19@gmail.com>
2022-09-18 20:56:36 +0530

    created NoteContext

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import NoteState from "./context/notes/NoteState";
 import About from "./pages/About";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <NoteState>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </NoteState>
+    </>
   );
 }
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import noteContext from "../context/notes/NoteContext";
 
 function About() {
-  return <div>About</div>;
+  const a = useContext(noteContext);
+  return <div>About {a.name + a.age}</div>;
 }
 
 export default About;
