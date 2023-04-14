820f084c16ed91c48cafc18fb4a1571d45d0bff4
shivansh-sagar <84396338+shivansh-sagar@users.noreply.github.com>
2023-04-14 17:09:24 +0530

    added url

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { message } from "antd";
 
 const NoteState = (props) => {
-  const host = "http://localhost:5000";
+  const host = "https://webnotes-api.onrender.com";
   const notesInitial = [];
   const [notes, setNotes] = useState(notesInitial);
 
